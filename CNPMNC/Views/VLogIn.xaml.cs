337cc540be99c4e4d171ebaf1fd46ecfd0b52e76
trivan77 <o868994610@gmail.com>
2025-03-22 15:40:20 +0700

    init commit

---
+++ CNPMNC/Views/VLogIn.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CNPMNC
+{
+     /// <summary>
+     /// Interaction logic for MainWindow.xaml
+     /// </summary>
+     public partial class VLogIn : Window
+     {
+          #region Var
+          bool isPWVisible = false;
+          #endregion
+
+          public VLogIn()
+          {
+               InitializeComponent();
+          }
+
+          private void txtUserName_TextChanged(object sender, TextChangedEventArgs e)
+          {
+               txtPlaceHolderUserName.Visibility = string.IsNullOrEmpty(txtUserName.Text) ? Visibility.Visible : Visibility.Collapsed;
+          }
+
+          private void pwBox_PasswordChanged(object sender, RoutedEventArgs e)
+          {
+               txtPlaceHolderPW.Visibility = string.IsNullOrEmpty(pwBox.Password) ? Visibility.Visible : Visibility.Collapsed;
+               txtPW.Text = pwBox.Password;
+          }
+
+          private void txtPW_TextChanged(object sender, TextChangedEventArgs e)
+          {
+               txtPlaceHolderPW.Visibility = string.IsNullOrEmpty(txtPW.Text) ? Visibility.Visible : Visibility.Collapsed;
+          }
+
+          private void btnHidePW_MouseDown(object sender, MouseButtonEventArgs e)
+          {
+               if (isPWVisible)
+               {
+                    // Ẩn mật khẩu: Chuyển về PasswordBox
+                    txtPW.Visibility = Visibility.Collapsed;
+                    pwBox.Visibility = Visibility.Visible;
+                    pwBox.Password = txtPW.Text;
+               }
+               else
+               {
+                    // Hiển thị mật khẩu: Chuyển sang TextBox
+                    txtPW.Visibility = Visibility.Visible;
+                    pwBox.Visibility = Visibility.Collapsed;
+                    txtPW.Text = pwBox.Password;
+               }
+
+               isPWVisible = !isPWVisible;
+          }
+
+          private void btnLogin_MouseEnter(object sender, MouseEventArgs e)
+          {
+               btnLogin.Background = new SolidColorBrush(Color.FromRgb(27, 70, 110));
+          }
+
+          private void btnLogin_MouseLeave(object sender, MouseEventArgs e)
+          {
+               btnLogin.Background = new SolidColorBrush(Color.FromRgb(18, 85, 147));
+          }
+     }
+}
---
+++ CNPMNC/Views/VMainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CNPMNC.Views
+{
+     /// <summary>
+     /// Interaction logic for VMainWindow.xaml
+     /// </summary>
+     public partial class VMainWindow : Window
+     {
+          public VMainWindow()
+          {
+               InitializeComponent();
+          }
+
+          private void NavbarButton_Click(object sender, RoutedEventArgs e)
+          {
+               btnOverview.IsChecked = false;
+               btnHumanResource.IsChecked = false;
+               btnStore.IsChecked = false;
+               btnTicket.IsChecked = false;
+
+               ToggleButton clickedButton = sender as ToggleButton;
+               if (clickedButton != null)
+               {
+                    clickedButton.IsChecked = true;
+               }
+          }
+     }
+}
